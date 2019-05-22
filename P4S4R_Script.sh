e807c5f0540a9adaade83602a08aac7b85a17d4e
riul-riblenk <50327463+riul-riblenk@users.noreply.github.com>
2019-05-23 06:54:25 +0700

    Add files via upload

---
+++ P4S4R_Script.sh
@@ -0,0 +1,209 @@
+#!/system/xbin/bash
+#MauNgapainGblk?
+#DiRecodeGakBakalJadiinLuMastah
+#DiGantiAuthotGakBakalJadiinLuMaster
+#MikirMakeOtakGblk
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+toilet -f big -F gay "PASAR" | lolcat
+toilet -f big -F gay "SCRIPT"
+echo ""
+echo "\033[32;1mAuthor : mr.farchan gans Feat Mr.Diary"
+echo "\033[35;1mTeam : ANONRABBIT CYBER TEAM"
+echo "\033[33;1mKontak Me :  081802899155"
+echo "\033[31;1mSuport saya gan"
+echo "\033[33;1mAnonrabbit Cyber Team" "\033[32;1m&" "\033[36;1mIndonesia cyber"
+echo "\033[34;1m[============================[>"
+sleep 1
+echo ""
+echo "\033[32;1mSilahkan Pilih Tools Yang mau Kamu pake:)?:"
+echo "\033[36;1m"
+echo "[===============================================[>"
+echo $green "1.> Spam Sms Tokopedia"
+echo "[===============================================]>"
+echo $purple "2.> Spam Sms Telkomnyet"
+echo "[===============================================]>"
+echo $cyan "3.> Spam Sms Matahari Mall"
+echo "[===============================================]>"
+echo $red "4.> Spam Sms HooqTV"
+echo "[===============================================]>"
+echo $white "5.> Spam Sms KFC"
+echo "[===============================================]>"
+echo $green "6.> Spam Sms PHD"
+echo "[===============================================]>"
+echo $purple "7.> Spam Sms Whiskas"
+echo "[===============================================]>"
+echo $cyan "8.> Spam Sms ZiPAY"
+echo "[===============================================]>"
+echo $red "9.> Nyari CC buat Carding"
+echo "[===============================================]>"
+echo $white "10.> Spam Akun Gmail"
+echo "[===============================================]>"
+echo $green "11.> Whois LookUp"
+echo "[===============================================]>"
+echo $purple "12.> Upload Admin Finder"
+echo "[===============================================]>"
+echo $cyan "13.> Script Deface Creator"
+echo "[===============================================]>"
+echo $red "14.> ReConDog Buat Information gathering"
+echo "[===============================================]>"
+echo $white "15.> Admin Login Finder"
+echo 
+"[==============================================]>"
+echo "\033[32;1m"
+read -p "[root@Pasar Script:~#" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet "Mr.farchan gans" | lolcat
+cd module
+php tp.php
+fi
+
+if
+[ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet "Mr.farchan gans"
+cd module
+php t.php                                                                                            fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+figlet "Mr.farchan gans"
+cd module
+php mataharimall.php
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+toilet "Mr.farcha gans"
+cd module
+php hooq.php
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+toilet -f slant --gay "Mr.farchan gans"
+cd module
+php kfc.php
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+toilet -f mono12 -F gay "Mr.farchan gans"
+cd module
+php phd.php
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+toilet "Mr.farchan gans"
+cd module
+php whiskas.php
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+figlet "Mr.farchan gans"
+cd module
+php zipay.php
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+toilet "Mr.farchan gans"
+cd module
+php key.php
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+toilet "Mr.farchan gans"
+cd module
+python2 SpamMail.py
+fi
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+clear
+toilet "Mr.farchan gans"
+cd module
+python2 whs.py
+fi
+
+if [ $bro = 12 ] || [ $bro = 12 ]
+then
+clear
+toilet "Mr.farchan gans"
+cd module
+python2 upf.py
+fi
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then
+clear
+toilet "Mr.farchan gans"
+cd module
+python2 create.py
+fi
+
+if [ $bro = 14 ] || [ $bro = 14 ]
+then
+clear
+toilet "Mr.farchan gans"
+cd module
+python2 dog.py
+fi
+
+if [ $bro = 15 ] || [ $bro = 15 ]
+then
+clear
+toilet "Mr.farchan gans"
+cd module
+python2 gam.py
+fi
+
+if [ $bro = 16 ] || [ $bro = 16 ]
+then
+clear
+toilet "Mr.farchan gans"
+cd module
+python2 anvima.py
+fi
+
+if [ $bro = 0 ] || [ $bro = 00 ]
+then
+echo "\033[32;1mANONRABBIT CYBER TEAM"
+sleep 1
+echo "\033[33;1mWe Security"
+sleep 1
+echo " We Not Friends"
+sleep 1
+echo "We Are Family"
+sleep 1
+echo "Hacking Is Not Criminal;)"
+sleep 1
+echo "Ketika Sebuah yang berjuang tidak di hargai"
+sleep 1
+echo "Maka Terus lah BerJuang Dan Berusaha:)"
+sleep 1
+echo "\033[32;1mKarna Suatu Hari Nanti Kamu akan Mendapatkannya:)"
+sleep 1
+exit
+fi
