b05d7955874aeffa365a944ffd71fc9a97c5ecd5
riul-riblenk <50327463+riul-riblenk@users.noreply.github.com>
2019-05-31 05:31:36 +0700

    Add files via upload

---
+++ framehack.sh
@@ -0,0 +1,132 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# Jangan Recode Ya gayn
+echo
+echo
+echo
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo $i" |"$cy"     SOLO VS SQUAD IS MY REAL"$i"                      |"
+echo $i" |"$me"─────────────────────────────────────────────"$i"|"
+echo $i" |"$pu" AUTHOR :"$ku" Tuan. f$Hans hans  [SOLO]"$i"           |"
+echo $i" |"$pu" FACEBOOK :"$ku" no name"$i"                       |"
+echo $i" |"$pu" THANKS TO :"$cy"~ [kawan] [teman] [pacar]"$i"     |"
+echo $i" |"$cy"            ~ [cyber] [%MR. Riuls] [Mr. Diary]"$i" |"
+echo $i" |"$pu" Contack WhatsApp :"$ku" 081802899155"$i"          |"
+echo $i" |"$pu" Contack Gmail :"$ku" cyberkids@gmail.com"$i"      |"
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo
+echo $i"|"$me" 1"$i" |"$cy" framehack FB PHP        "$i"|"$i"  f  |"$cy" "$i" |"
+echo $i"|"$me" 2"$i" |"$cy" framehack FB BR1G4D3    "$i"|"$i"  r  |"$cy" "$i" |"
+echo $i"|"$me" 3"$i" |"$cy" framehack FB iqbalz     "$i"|"$i"  a  |"$cy" "$i" |"
+echo $i"|"$me" 4"$i" |"$cy" framehack FB MBF        "$i"|"$i"  m  |"$cy" "$i" |"
+echo $i"|"$me" 5"$i" |"$cy" framehack N1ght420      "$i"|"$i"  e  |"$cy" "$i" |"
+echo $i"|"$me" 6"$i" |"$cy" framehack FB ( root )   "$i"|"$i"  h  |"$cy" "$i" |"
+echo $i"|"$me" 7"$i" |"$cy" INSTALL BAHAN DULU Bos "$i" |"$i"  c  |"$cy" "$i" |"
+echo $i"|"$me" 8"$i" |"$cy" KELUAR/EXIT PROGRAM     "$i"|"$i"  k  |"$cy" "$i" |"
+echo
+echo $me"┌==="$bi"["$i"Sendiriguys"$bi"]"$me"======"$bi"["$i""Pilih Nomornya""$bi"]"
+echo $me"¦"
+read -p"└──# " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/fbbrute
+cd fbbrute
+php fb.php
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/Fb-Cracker-v.3
+cd Fb-Cracker-v.3
+python2 crack.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/IqbalzNoobs/fb-brute
+cd fb-brute
+python2 brute.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/N1ght420/FaceBrute
+cd FaceBrute
+python fb.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/facebash.git
+cd facebash
+bash facebash.sh
+fi
+
+if [ $pil = 7 ]
+then
+clear
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant " S U K S E S "|lolcat
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo $cy"Terima Kasih Sudah Pakai Tool Saya"
+sleep 2
+echo $i"Bila Ada Kesalahan Kamu Bisa Nanya Kepada Saya"
+sleep 2
+echo $ku"WhatsApp :"$i" 081802899155"
+echo $ku"Facebook :"$i" farchan anugrah"
+sleep 2
+echo $pur">> Thanks Yang Sudah Support Saya <<"
+exit
+fi
